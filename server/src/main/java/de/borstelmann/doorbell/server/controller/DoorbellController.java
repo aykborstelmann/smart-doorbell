0acf707e6d6963e6095b3345e1f9aa56eb1d8e66
Ayk Borstelmann <ayk.borstelmann@cronn.de>
2022-03-24 21:49:22 +0100

    #17: Implement DoorbellController

--- server/src/main/java/de/borstelmann/doorbell/server/controller/DoorbellController.java
+++ server/src/main/java/de/borstelmann/doorbell/server/controller/DoorbellController.java
@@ -1,8 +1,12 @@
 package de.borstelmann.doorbell.server.controller;
 
+import de.borstelmann.doorbell.server.domain.model.DoorbellDevice;
 import de.borstelmann.doorbell.server.openapi.api.DoorbellApi;
 import de.borstelmann.doorbell.server.openapi.model.DoorbellRequest;
 import de.borstelmann.doorbell.server.openapi.model.DoorbellResponse;
+import de.borstelmann.doorbell.server.services.DoorbellService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,26 +15,48 @@ import java.util.List;
 
 @RequestMapping("/api/v1")
 @RestController
+@RequiredArgsConstructor
 public class DoorbellController implements DoorbellApi {
+
+    private final DoorbellService doorbellService;
+    private final ModelMapper modelMapper;
 
     @Override
     public ResponseEntity<DoorbellResponse> createDoorbell(Long userId, DoorbellRequest doorbellRequest) {
-        return null;
+        DoorbellDevice doorbellDevice = convertToDomainModel(doorbellRequest);
+        DoorbellDevice doorbell = doorbellService.createDoorbell(userId, doorbellDevice);
+        DoorbellResponse doorbellResponse = convertToResponse(doorbell);
+        return ResponseEntity.ok(doorbellResponse);
     }
 
     @Override
     public ResponseEntity<List<DoorbellResponse>> getAllDoorbells(Long userId) {
-        return null;
+        List<DoorbellDevice> doorbells = doorbellService.getAllDoorbells(userId);
+        List<DoorbellResponse> doorbellResponses = doorbells.stream()
+                .map(this::convertToResponse)
+                .toList();
+        return ResponseEntity.ok(doorbellResponses);
     }
 
     @Override
     public ResponseEntity<DoorbellResponse> getDoorbell(Long doorbellId) {
-        return null;
+        DoorbellDevice doorbell = doorbellService.getDoorbell(doorbellId);
+        DoorbellResponse doorbellResponse = convertToResponse(doorbell);
+        return ResponseEntity.ok(doorbellResponse);
     }
 
     @Override
     public ResponseEntity<Void> deleteDoorbell(Long doorbellId) {
-        return null;
+        doorbellService.deleteDoorbell(doorbellId);
+        return ResponseEntity.noContent().build();
+    }
+
+    private DoorbellResponse convertToResponse(DoorbellDevice doorbell) {
+        return modelMapper.map(doorbell, DoorbellResponse.class);
+    }
+
+    private DoorbellDevice convertToDomainModel(DoorbellRequest doorbellRequest) {
+        return modelMapper.map(doorbellRequest, DoorbellDevice.class);
     }
 
 }
--- server/src/main/java/de/borstelmann/doorbell/server/error/NotFoundException.java
+++ server/src/main/java/de/borstelmann/doorbell/server/error/NotFoundException.java
@@ -1,21 +1,19 @@
 package de.borstelmann.doorbell.server.error;
 
 import org.jetbrains.annotations.NotNull;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 public class NotFoundException extends RuntimeException {
     public NotFoundException(String message) {
         super(message);
     }
 
-    @ResponseBody
-    @Override
-    public String getMessage() {
-        return super.getMessage();
-    }
-
     @NotNull
     public static NotFoundException createUserNotFoundException(long userId) {
         return new NotFoundException(String.format("User with ID %d not found", userId));
+    }
+
+    @NotNull
+    public static NotFoundException createDoorbellNotFoundException(long doorbellId) {
+        return new NotFoundException(String.format("Doorbell with ID %d not found", doorbellId));
     }
 }
