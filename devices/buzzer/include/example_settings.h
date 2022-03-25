e98f0430f8ad04f8d9ba0c2d49c03edaa30988c1
Ayk Borstelmann <ayk.borstelmann@cronn.de>
2022-03-25 11:44:45 +0100

    #5: Add platformio project and configure for websockets

---
+++ devices/buzzer/include/example_settings.h
@@ -0,0 +1,12 @@
+#ifndef BUZZER_SETTINGS_H
+#define BUZZER_SETTINGS_H
+
+const char *wlan_ssid = "yourssid";
+const char *wlan_password = "somepassword";
+
+const char *ws_host = "the.host.net";
+const int ws_port = 80;
+
+const char *deviceId = "0";
+
+#endif //BUZZER_SETTINGS_H
